9178013480d4b8e0f8b76abfb65bd6d0e992f958
thomas <thomas@thomasui-MacBookPro.local>
2025-06-02 10:06:34 +0900

    Three ways to select a specific character

---
+++ selectCharacter.js
@@ -0,0 +1,8 @@
+let a = "abcdefghijklmnopqrstuvwxyz";
+console.log(a[0]); // a
+console.log(a.charAt(1)); // b
+console.log(a.at(2)); // c
+
+console.log(a[-3]); // undefined
+console.log(a.charAt(-3)); //
+console.log(a.at(-3)); // x
