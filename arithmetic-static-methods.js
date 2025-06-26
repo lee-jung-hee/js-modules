4f6508fb4b06d30873910437177abdf0dceea513
lee-jung-hee <zhongxi710@gmail.com>
2025-06-26 10:58:59 +0900

    Creating basic arithmetic methods using static methods

---
+++ arithmetic-static-methods.js
@@ -0,0 +1,11 @@
+class MathUtils {
+  static add = (a, b) => a + b;
+  static subtract = (a, b) => a - b;
+  static multiply = (a, b) => a * b;
+  static divide = (a, b) => a / b;
+}
+
+console.log(MathUtils.add(2, 3)); // 5
+console.log(MathUtils.subtract(5, 3)); // 2
+console.log(MathUtils.multiply(4, 5)); // 20
+console.log(MathUtils.divide(20, 5)); // 4
