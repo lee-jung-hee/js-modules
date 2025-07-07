c5d39fa258cae38bf6359b7359153469a8c34acb
lee-jung-hee <zhongxi710@gmail.com>
2025-07-07 16:53:14 +0900

    A function in JavaScript that returns a new set containing only the elements common to two sets

---
+++ find-intersection-set-items.js
@@ -0,0 +1,8 @@
+const set1 = new Set([1, 2, 3, 4, 5]);
+const set2 = new Set([1, 2, 3]);
+
+function findIntersection(set1, set2) {
+  return new Set([...set1].filter((item) => set2.has(item)));
+}
+
+console.log(findIntersection(set1, set2));
